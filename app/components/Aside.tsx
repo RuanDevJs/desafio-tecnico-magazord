19c05a4daf6de73e8b7e62a25b179eccb5da4024
RuanDevJs <ruanvelpidio@hotmail.com>
2025-12-01 10:42:28 -0300

    feature: Adicionado responsivo pra mobile (iphone) e accordion pras informações de perfil

--- app/components/Aside.tsx
+++ app/components/Aside.tsx
@@ -2,9 +2,11 @@
 
 import { Buildings, InstagramLogo, LinkSimple, MapPin } from "phosphor-react"
 import useAccount from "../store/useAccount"
+import { useState } from "react";
 
 export default function Aside() {
   const account = useAccount(store => store.state.account);
+  const [showAccount, setShowAccount] = useState(false);
 
   return (
     <aside>
@@ -17,7 +19,7 @@ export default function Aside() {
           <p className="md:text-sm lg:text-lg font-normal text-center text-zinc-500">{account?.bio}</p>
         </div>
         <div className="mt-3">
-          <ul className="flex md:ml-5 flex-col gap-3 ml-6">
+          <ul className="flex md:ml-5 flex-col gap-3 ml-6" id="profile-information-web">
             <li>
               <a href="#" className="text-sm font-normal flex items-center gap-2.5 text-blue-500">
                 <Buildings size={23} weight="light" />
@@ -43,6 +45,35 @@ export default function Aside() {
               </a>
             </li>
           </ul>
+          <div className="my-3.5">
+            <p className="text-base text-[#0587FF] font-normal text-center mb-3.5" onClick={() => setShowAccount(oldValue => !oldValue)}>Informações Adicionais</p>
+            {showAccount ? <ul className="flex md:ml-5 flex-col gap-3 bg-[#f2f2f2] w-[320px] m-auto p-3.5 rounded-lg">
+              <li>
+                <a href="#" className="text-sm font-normal flex items-center gap-2.5 text-blue-500">
+                  <Buildings size={23} weight="light" />
+                  Magazord - plataforma
+                </a>
+              </li>
+              {account?.location && (<li>
+                <a href="#" className="text-sm font-normal flex items-center gap-2.5 text-blue-500">
+                  <MapPin size={23} weight="light" />
+                  {account?.location}
+                </a>
+              </li>)}
+              {account?.blog && (<li>
+                <a href={account?.blog} className="text-sm font-normal flex items-center gap-2.5 text-blue-500">
+                  <LinkSimple size={23} weight="light" />
+                  Portfolio
+                </a>
+              </li>)}
+              <li>
+                <a href="#" className="text-sm font-normal flex items-center gap-2.5 text-blue-500">
+                  <InstagramLogo size={23} weight="light" />
+                  {account?.login}
+                </a>
+              </li>
+            </ul> : null}
+          </div>
         </div>
       </div>
     </aside>
--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -6,7 +6,7 @@ import Repositories from '../../components/Profile/Repositories';
 
 export default function Profile() {
   return (
-    <main className='sm:w-[60%] md:w-[80%] lg:w-4/5  m-auto'>
+    <main className='sm:w-[60%] md:w-[80%] lg:w-4/5  m-auto' id="profile">
       <div className='grid md:grid-cols-[220px_1fr] 2xl:grid-cols-[300px_1fr] gap-8 mt-10 sm:grid-cols-1'>
         <Aside />
         <div>
